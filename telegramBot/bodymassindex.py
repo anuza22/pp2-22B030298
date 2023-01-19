f85ac454e753b957d53bd1ef47e0a51f5f5f3f2d
anuza22 <a_akturina@kbtu.kz>
2023-01-19 13:01:33 +0600

    Second commit

--- telegramBot/bodymassindex.py
+++
@@ -1,9 +0,0 @@
-import telebot
-
-bot = telebot.TeleBot("5830481540:AAEO4NvLEwg9an6dX2rBdfDWlx2mM5e4", parse_mode=None)
-
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, 'Hello')
-
-bot.polling(none_stop=True)
--- train/1.py
+++
@@ -1,9 +0,0 @@
-a = input()
-def solution(string):
-    out = ''
-    for i in range(len(string)-1, -1, -1):
-       out+=string[i]
-    return out 
-
-
-print(solution(a))
